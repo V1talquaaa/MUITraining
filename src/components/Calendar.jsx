a909de544931ec72023f9abc039dfbaa5d5ee65c
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-08-28 13:41:41 +0200

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -24,11 +24,29 @@ const Calendar = () => {
         light: '#ff7961',
         main: '#f44336',
         dark: '#ba000d',
-        contrastText: '#000',
+        contrastText: '#fff',
       },
     },
-
-
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            backgroundColor: 'transparent',
+          }
+        }
+      },
+    MuiPickersDay: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "red",
+          color: "#fff"
+        }
+      }
+    },
+    MuiDayPicker: {
+      
+    }
+    }
   });
 
   const [value, setValue] = useState(dayjs(date));
@@ -53,6 +71,7 @@ const Calendar = () => {
           label="Deadline"
           views={['month', 'day']}
           disablePast
+          
           onChange={newValue => setValue(newValue)}
           showDaysOutsideCurrentMonth
           renderInput={props => <TextField {...props}/>}
@@ -60,8 +79,10 @@ const Calendar = () => {
             svg: {fill: '#BEDBB0'},
             input: {color: '#BEDBB0'},
             label: {color: 'rgba(255, 255, 255, 0.50)'},
+            calendarHeader: {backgroundColor: "#FF0000"},
             "& .MuiOutlinedInput-notchedOutline": { border: 0, "&:focus": {border: 0}},
-            "& .MuiPaper-root": {backgroundColor: "#151515"}
+            "& .css-1yllih9-MuiPaper-root-MuiPickersPopper-paper": {backgroundColor: "#FF0000"},
+            "& .MuiPickersPopper-root": {backgroundColor: "#FF0000"}
 
           }}
         />
@@ -72,5 +93,114 @@ const Calendar = () => {
 
 export { Calendar };
 
+
+// import { DatePicker } from '@mui/x-date-pickers';
+// import dayjs from 'dayjs';
+// import * as React from 'react';
+// import { useState } from 'react';
+// import css from './Calendar.module.css'
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import { TextField } from '@mui/material';
+
+
+
+// const Calendar = () => {
+//   const date = new Date();
+
+//   const theme = createTheme({
+//     components: {
+//       MuiPaper: {
+//         styleOverrides: {
+//           root: {
+//             backgroundColor: "transparent"
+//           }
+//         }
+//       },
+
+//       MuiCalendarPicker: {
+//         styleOverrides: {
+//           root: {
+//             backgroundColor: "yellow",
+//             color: "red"
+//           }
+//         }
+//       },
+//       MuiDayPicker: {
+//         styleOverrides: {
+//           header: {
+//             backgroundColor: "yelow",
+//             color: "green"
+//           }
+//         }
+//       },
+//       MuiPickersDay: {
+//         styleOverrides: {
+//           root: {
+//             backgroundColor: "green",
+//             color: "yellow"
+//           },
+//           today: {
+//             backgroundColor: "orange",
+//             color: "red"
+//           },
+//         }
+//       },
+//     }
+//   });
+
+//   const [value, setValue] = useState(dayjs(date));
+
+//   return (
+//     <div 
+//     style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}>
+//     <ThemeProvider theme={theme}>
+//         <DatePicker
+//           selected className={css.MenuItem}
+//           components={{
+//             OpenPickerIcon: ExpandMoreIcon  
+//         }}
+//           value={value}
+//           label="Deadline"
+//           views={['month', 'day']}
+//           disablePast
+//           slotProps={{
+//             actionBar: {
+//               actions: ['today']
+//             },
+//             toolbar: {
+//               hidden: false,
+//               toolbarPlaceholder: "today",
+//               toolbarFormat: "dd, mm"
+//             }
+//           }}
+          
+//           onChange={newValue => setValue(newValue)}
+//           showDaysOutsideCurrentMonth
+//           renderInput={props => <TextField {...props}/>}
+//           sx={{
+//             svg: {fill: '#BEDBB0'},
+//             input: {color: '#BEDBB0'},
+//             label: {color: 'rgba(255, 255, 255, 0.50)'},
+//             calendarHeader: {backgroundColor: "#FF0000"},
+//             "& .MuiOutlinedInput-notchedOutline": { border: 0, "&:focus": {border: 0}},
+//             "& .css-1yllih9-MuiPaper-root-MuiPickersPopper-paper": {backgroundColor: "#FF0000"},
+//             "& .MuiPickersPopper-root": {backgroundColor: "#FF0000"}
+
+//           }}
+//         />
+// </ThemeProvider>
+//     </div>
+//   );
+// };
+
+// export { Calendar };
 
 
